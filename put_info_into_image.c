2fbb56169ebfe9b13773b2e8a58468b859311406
Ismail Raqi <iraqi@e1r7p3.1337.ma>
2022-04-24 04:53:47 +0000

    done

--- put_info_into_image.c
+++ put_info_into_image.c
@@ -6,7 +6,7 @@
 /*   By: iraqi <iraqi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 05:01:00 by iraqi             #+#    #+#             */
-/*   Updated: 2022/04/23 06:52:36 by iraqi            ###   ########.fr       */
+/*   Updated: 2022/04/24 04:53:31 by iraqi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	put_strings_to_image(t_data *data)
 
 	start = WIDTH - M_WIDTH;
 	mlx_string_put(data->mlx, data->win, start, 5, \
-	0x00000000, "=> USE ( X / Y / Z )");
+	0x00000000, "=> USE ( X / Z / Y )");
 	mlx_string_put(data->mlx, data->win, start, 20, \
 	0x008c8a82, "TO ROTATE THE SHAPE ( CLOCKWISE )");
 	mlx_string_put(data->mlx, data->win, start, 50, \
@@ -33,4 +33,8 @@ void	put_strings_to_image(t_data *data)
 	0x00000000, "=> USE MOUSE SCROLL WHEEL ( UP / DOWN)");
 	mlx_string_put(data->mlx, data->win, start, 155, \
 	0x008c8a82, "ZOOM IN AND OUT");
+	mlx_string_put(data->mlx, data->win, start, 185, \
+	0x00000000, "=> USE ( P / I ) TO SWITCH PROJECTION");
+	mlx_string_put(data->mlx, data->win, start, 200, \
+	0x008c8a82, "P ( PARALLEL ) | I ( ISO )");
 }
